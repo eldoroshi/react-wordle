59091c3718545144983c170b5c68e1d38b2b2b40
Eldo Roshi <eldoroshi@users.noreply.github.com>
2022-05-21 00:49:05 +0200

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,29 +1,29 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
-export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
-export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const WIN_MESSAGES = ['¡Buen trabajo!', 'Impresionante', '¡Bien hecho!']
+export const GAME_COPIED_MESSAGE = 'Juego copiado al portapapeles'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'No hay suficientes letras'
+export const WORD_NOT_FOUND_MESSAGE = 'Palabra no encontrada'
 export const HARD_MODE_ALERT_MESSAGE =
-  'Hard Mode can only be enabled at the start!'
+  'El modo difícil solo se puede habilitar al principio!'
 export const HARD_MODE_DESCRIPTION =
-  'Any revealed hints must be used in subsequent guesses'
-export const HIGH_CONTRAST_MODE_DESCRIPTION = 'For improved color vision'
+  'Cualquier pista revelada debe usarse en conjeturas posteriores'
+export const HIGH_CONTRAST_MODE_DESCRIPTION = 'Para mejorar la vista del color'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `The word was ${solution}`
+  `La palabra era ${solution}`
 export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
-  `Must use ${guess} in position ${position}`
+  `Debe usar ${guess} en la posición ${position}`
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
-  `Guess must contain ${letter}`
+  `Supongo que debe contener ${letter}`
 export const ENTER_TEXT = 'Enter'
 export const DELETE_TEXT = 'Delete'
-export const STATISTICS_TITLE = 'Statistics'
-export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
-export const NEW_WORD_TEXT = 'New word in'
-export const SHARE_TEXT = 'Share'
-export const TOTAL_TRIES_TEXT = 'Total tries'
-export const SUCCESS_RATE_TEXT = 'Success rate'
-export const CURRENT_STREAK_TEXT = 'Current streak'
-export const BEST_STREAK_TEXT = 'Best streak'
+export const STATISTICS_TITLE = 'Estadísticas'
+export const GUESS_DISTRIBUTION_TEXT = 'Distribución de conjeturas'
+export const NEW_WORD_TEXT = 'Nueva palabra en'
+export const SHARE_TEXT = 'Compartir Resultados'
+export const TOTAL_TRIES_TEXT = 'Intentos totales'
+export const SUCCESS_RATE_TEXT = 'Tasa de éxito'
+export const CURRENT_STREAK_TEXT = 'Racha actual'
+export const BEST_STREAK_TEXT = 'Mejor racha'
 export const DISCOURAGE_INAPP_BROWSER_TEXT =
-  "You are using an embedded browser and may experience problems sharing or saving your results. We encourage you rather to use your device's default browser."
+  "Est� utilizando un navegador integrado y puede tener problemas para compartir o guardar sus resultados. Le recomendamos que utilice el navegador predeterminado de su dispositivo."
